568d1b779a272667d626b9435aaa5c2df1f0b600
Undercity1914 <mlb.marcos22@gmail.com>
2025-12-08 21:43:59 -0300

    new content on navbar

--- frontend/ecommerce-frontend/src/app/home/page.tsx
+++ frontend/ecommerce-frontend/src/app/home/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/navbar";
 
 export default function Home(){
     return(
-        <div className="flex flex-col justify-between w-screen h-screen bg-linear-to-b from-[#e92727] to-black">
+        <div className="flex flex-col justify-between w-screen h-screen bg-linear-to-b from-white to-black">
             <Navbar/>
         </div>
     );
